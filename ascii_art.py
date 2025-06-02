b46b6ff161f3e0c06eeb86ed7e7755c963b5dada
Tejas <tejasdilipkamble@gmail.com>
2025-06-02 13:59:21 +0530

    basic ascii art done final

--- ascii_art.py
+++ ascii_art.py
@@ -1,30 +1,35 @@
 from PIL import Image 
 import numpy as np
 
+ASCII_CHARS = "@%#*+=-:. "
+MAX_PIXEL_VALUE = 255
+
 im = Image.open("car.jpeg")
+im = im.convert("L")
 
-resized_im = im.resize((640,480))
+resized_im = im.resize((480, 280))
 resized_im.save("resized_img.jpeg")
-
-print(f"Image Size : {resized_im.size}")
 
 pixel_matrix = np.array(resized_im)
 
+intensity_matrix = pixel_matrix.tolist() 
 
-def get_br_matrix(matrix) :
-    intensity_matrix = []
-    for row in matrix :
-        intensity_row = []
-        for p in row :
-            intensity = (p[0] + p[1] + p[2])/3.0
-            intensity_row.append(intensity)
-        intensity_matrix.append(intensity_row)
-    return intensity_matrix        
-
-print(get_br_matrix(pixel_matrix))
-
+def convert_to_ascii(intensity_matrix, ascii_chars):
+    ascii_matrix = []
+    for row in intensity_matrix:
+        ascii_row = []
+        for p in row:
+                index = int(p / MAX_PIXEL_VALUE * (len(ascii_chars) - 1))
+                ascii_row.append(ascii_chars[index])
+        ascii_matrix.append(ascii_row)
+    return ascii_matrix
 
 
+print(convert_to_ascii(intensity_matrix, ASCII_CHARS))
 
+def print_ascii_matrix(ascii_matrix):
+    for row in ascii_matrix:
+        line = [p*3 for p in row]
+        print("".join(line))
 
-    
+print_ascii_matrix(convert_to_ascii(intensity_matrix, ASCII_CHARS))    
